e49bacec7aa21aeb239028d6ce588b0c04274e62
shubham jain <sj27754@gmail.com>
2020-03-29 22:42:14 +0530

    Create Folder

--- COVID19/createcsv.py
+++ COVID19/createcsv.py
@@ -5,11 +5,12 @@ import numpy as np
 
 #create data using random module
 idx = np.arange(100)
-l=[]
+fever_list=[]
 for i in range(2500):
-	d='{:04.3f}'.format(random.uniform(93, 104))
-	l.append(d)
-fever = np.array(l)
+	fever_data ='{:04.3f}'.format(random.uniform(93, 104))
+	fever_list.append(fever_data)
+fever = np.array(fever_list)
+
 #fever = np.random.randint(92.0,104.0,size=2500)
 bodyPain  = np.random.randint(2,size=2500)
 age  = np.random.randint(20,80,size=2500)
